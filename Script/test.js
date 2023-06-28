e57c36b502a48d44b1c0f3dbbb64a9d890154e44
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-06-28 14:36:49 +0200

    then -> await

--- Script/test.js
+++ Script/test.js
@@ -25,31 +25,25 @@ async function get_users_from_role(role_name) {
     */
     let list = []
 
-    get_db_from_name("Role").then(data => {
-        // Find where to look for
-        let role_data = data.find((elmt) => elmt.name == role_name)
-        let role_category = role_data.category
-        console.log(role_data)
+    let data = await get_db_from_name("Role")
+    let role_data = data.find((elmt) => elmt.name == role_name)
+    let role_category = role_data.category
 
-        // Get all data from that category
-        get_db_from_name(role_category).then(data => {
-            data.forEach(elmt => {
+    let data = await get_db_from_name(role_category)
+    data.forEach(elmt => {
 
-                // Did this person ever get the role?
-                if (elmt[role_name] != null) {
-                    // Yes, add to list
-                    list.push({
-                        name: elmt.name,
-                        time: elmt[role_name],
-                        current: elmt.current
-                    })
-                }
+        // Did this person ever get the role?
+        if (elmt[role_name] != null) {
+            // Yes, add to list
+            list.push({
+                name: elmt.name,
+                time: elmt[role_name],
+                current: elmt.current
             })
-            console.log(list)
-        })
+        }
     })
 
     return list
 }
 
-get_users_from_role("helper").then(data => console.log(data))
+get_users_from_role("guardianmanager").then(data => console.log(data))
