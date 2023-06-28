bf5d048328ada142c71a9d926512fbd65c253959
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-06-28 13:05:51 +0200

    Easier to read log

--- Script/test.js
+++ Script/test.js
@@ -7,7 +7,7 @@ async function get_db_from_name(db_name) {
 get_db_from_name("Discords").then(data => {
     data.forEach(elmt => {
         if (elmt.current != "resigned") {
-            console.log(elmt)
+            console.log(elmt.name)
         }
     })
 })
