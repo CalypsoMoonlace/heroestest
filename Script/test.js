8832bc827ca4bcda80f40682e9762096f5304544
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-06-28 14:38:22 +0200

    Fix

--- Script/test.js
+++ Script/test.js
@@ -25,7 +25,7 @@ async function get_users_from_role(role_name) {
     */
     let list = []
 
-    let data = await get_db_from_name("Role")
+    let role_db = await get_db_from_name("Role")
     let role_data = data.find((elmt) => elmt.name == role_name)
     let role_category = role_data.category
 
