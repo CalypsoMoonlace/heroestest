dd2c4073eac60c625bbd48b047a006463c6f5e2e
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-06-29 16:58:58 +0200

    relative path

--- Script/test.js
+++ Script/test.js
@@ -1,5 +1,5 @@
 async function get_db_from_name(db_name) {
-    const response = await fetch(`./data/${db_name}.json`)
+    const response = await fetch(`./Script/data/${db_name}.json`)
     const jsonData = await response.json()
     return jsonData
 }
