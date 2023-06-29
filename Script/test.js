1164758e60e2fea7fbaf3ba4e3780d1a649d5972
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-06-29 13:53:40 +0200

    bug fix

--- Script/test.js
+++ Script/test.js
@@ -62,7 +62,7 @@ async function get_roles_from_user(user_name) {
     "Boon" returns { roles: [ {name: "trialhelper", time: 1542932401}, {name: "trialhelper", time: 1583085566}, ... ], current: ["resigned"]}
     "invalid_user_name" returns { roles: [], current: []}
     */
-    let result = { roles: [], current: []}
+    let result = { roles: [], current: [] }
 
     // Find which db to look into
     let member_db = await get_db_from_name("Member")
@@ -70,7 +70,7 @@ async function get_roles_from_user(user_name) {
     if (!member_data) { return result } // invalid name, no data
     let member_categories = role_data.categories.split(" ") // => eg: member_categories = ["Discord", "Mentor"]
 
-    member_categories.forEach(category => {
+    member_categories.forEach(async category => {
         // Add category by category
         let category_db = await get_db_from_name(category)
         let category_data = category_db.find((elmt) => elmt.name == user_name)
