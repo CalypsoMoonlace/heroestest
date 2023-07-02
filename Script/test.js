0019d90ab35d01cef131ba5da444aecfcbadfdaf
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-07-02 16:50:22 +0200

    bug fix

--- Script/test.js
+++ Script/test.js
@@ -490,7 +490,7 @@ function add_rank_link(role_db) {
     // pre: body is loaded, role_db is the data from the Role database and all role_link elements contain the text of a role
     // post: adds a link & the colour of the role
     Array.from(document.getElementsByClassName("role_link")).forEach(link => {
-        let role_data = role_db.find(elmt => elmt.name.toLowerCase() == link.innerText.toLowerCase())
+        let role_data = role_db.find(elmt => elmt.display_name.toLowerCase() == link.innerText.toLowerCase())
         link.href = `?role=${link.innerText}`
         link.color = role_data.colour
     })
