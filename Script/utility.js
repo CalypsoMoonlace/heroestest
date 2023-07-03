9123b38d38b72b58b0572742932a102c0552d2f5
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-07-03 21:24:32 +0200

    debugging

--- Script/utility.js
+++ Script/utility.js
@@ -48,9 +48,12 @@ function add_rank_link(role_db) {
             let role_data = role_db.find(elmt => elmt.display_name.toLowerCase() == text)
         }
 
+        console.log(role_data)
+        
         if (!role_data) {
             // if still nothing found, try again with regular name
             let role_data = role_db.find(elmt => elmt.name.toLowerCase() == text)
+            console.log(role_data)
         }
 
         if (role_data && !link.href) {
