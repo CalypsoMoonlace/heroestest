e569d42c52e396d406c0f0056ceaae6c70ee8536
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-07-03 18:17:01 +0200

    temporarily adding unix to date before I put it into a shared file/module

--- Script/testupdates.js
+++ Script/testupdates.js
@@ -185,3 +185,23 @@ async function load_updates(amount_to_load) {
     // update loaded data
     amount_loaded += amount_to_load
 }
+
+function unix_to_date(timestamp) {
+    // pre: timestamp is a unix timestamp from the database
+    // post: returns "dd month yyyy"
+    // example: 0 -> "01 January 1900"
+    let date_obj = new Date(timestamp*1000); // from unix to object
+    
+    day = date_obj.getDate()
+    if (day<10) {
+        day = "0" + day
+    }
+
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    month = months[date_obj.getMonth()] // going from number to string
+
+    year = date_obj.getYear() + 1900
+
+    result = day + " " + month + " " + year
+    return result
+}
