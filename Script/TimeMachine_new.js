140e8649ffa1323c58b0a6183cf9b09b9a859189
Arnaud <68601656+Arnaud0865@users.noreply.github.com>
2023-08-09 17:34:38 +0200

    Dynamic generation of role list

--- Script/TimeMachine_new.js
+++ Script/TimeMachine_new.js
@@ -7,6 +7,8 @@ async function get_users_from_role(role_name, timestamp) {
     Example:
     "guardian" returns [ {name: Lisa, time: 1598006832, current: "guardianmanagerhelper", languages: ["German", "Dutch"]}, ...]
     "invalid_role_name" returns []
+
+    Note: this function is essentially the same as the one in list.js except for the timestamp argument & check
     */
     let list = []
 
@@ -49,3 +51,76 @@ async function get_users_from_role(role_name, timestamp) {
     return list
 }
 
+async function loading() {
+    /*
+    pre: body is loaded
+    post: adds all relevant information on body by initialising TimeMachine to current time
+    */
+    let unix_today = (new Date()).getTime() / 1000; // today in unix
+    let role_db = await get_db_from_name("Role")
+
+    for (var i = 0; i < role_db.length; i++) {
+        let role = role_db[i]
+        console.log(role.name)
+
+        // For each role, generate the following: 
+        /*
+        <div class="megamod rang">
+            <div class="megamod_titre">
+                <a class="role_link">Megamod</a>
+            </div>
+            <div id="megamod">
+            </div>
+        </div>
+        */
+        // NB: This is not a pretty way of doing it, but it's better than before
+
+        // Structure
+        let role_html = document.createElement('div')
+        role_html.classList = `${role.name} rang`
+
+        let role_title = document.createElement('div')
+        role_title.classList = "titre"
+
+        let role_link = `<a class="role_link">${role.display_name}</a>`
+        role_title.innerHTML = role_link
+        role_html.appendChild(role_title)
+
+        // User list
+        let user_list = `<div id=${role.name}></div>`
+        role_html.innerHTML += user_html
+
+        // Add to the rest
+        let category_html = document.getElementsByName(role.category)[0]
+        category_html.appendChild(role_html)
+    }
+
+    //load_machine_from_stamp(unix_today);
+
+    // Add role & member links
+    add_rank_link(role_db)
+    add_member_link()
+}
+
+async function load_machine_from_stamp(timestamp) {
+    /*
+    pre: body is loaded, timestamp is the unix timestamp of the TimeMachine's time
+    post: removes all previous names, adds new names and hides roles that don't contain any users
+    */
+    let role_db = await get_db_from_name("Role")
+
+    for (var i = 0; i < role_db.length; i++) {
+        let role = role_db[i]
+        let users = get_users_from_role(role.name,timestamp)
+
+        users_data.sort((a,b) => a.time - b.time) // sort by time
+
+        users_data.forEach(user => {
+            // Add to HTML
+            let user_html = `<a class="name_link">${user.name}</a>`;
+            document.getElementById(role.name).innerHTML += user_html;
+        })
+    }
+
+    document.getElementById('date').innerText = `Staff on the ${unix_to_date(timestamp)}`
+}
